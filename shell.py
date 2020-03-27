473903cf7747c38d24e818ea5c47cbed1dabb6e7
HildegunnHana <59610518+HildegunnHana@users.noreply.github.com>
2020-03-27 19:55:14 +0100

    Update shell.py

--- shell.py
+++ shell.py
@@ -1,5 +1,5 @@
 import network
 ap = network.WLAN(network.AP_IF)
 ap.active(True)
-ap.config(essid='uPY_14n', authmode=network.AUTH_WPA_WPA2_PSK)
+ap.config(essid='navnet_du_vil_ha', authmode=network.AUTH_WPA_WPA2_PSK)
 print (ap.ifconfig())
